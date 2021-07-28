d4a70029cd27d7df22219997862b0a5b8478994d
Nicolas Barcellos <nicolasbarcellos95@gmail.com>
2021-07-27 21:54:19 -0300

    first commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,15 @@
+import { ButtonHTMLAttributes } from "react";
+
+import "../styles/button.scss";
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  isOutlined?: boolean;
+};
+
+export default function Button({ isOutlined = false, ...props }: ButtonProps) {
+  return (
+    <button className={`button ${isOutlined ? "outlined" : ""}`} {...props}>
+      {props.children}
+    </button>
+  );
+}
